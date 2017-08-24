e3f949ccdb0e77a07aa01d7c9e311242964d2bd2
DESKTOP-57QE3ET\Matheus <matheus-gbn@hotmail.com>
2017-08-23 21:08:15 -0300

    Organizando controller lista

--- controllers/listaController.js
+++ controllers/listaController.js
@@ -1,17 +1,17 @@
 app.controller("listaController", function($scope, $http, $location){
-	$scope.maxSize = 12;
-	$scope.bigCurrentPage = 1;      		
+    $scope.maxSize = 12;
+    $scope.bigCurrentPage = 1;      		
 
     $scope.pageChanged = function() {
     	$scope.buscarRepositorios();
     }; 
 
-	$scope.buscarRepositorios = function(){
-		$http.get("https://api.github.com/search/repositories?q=is:public&page="+ $scope.bigCurrentPage + "&per_page=12&").then(function (response) {
+    $scope.buscarRepositorios = function(){
+    	$http.get("https://api.github.com/search/repositories?q=is:public&page="+ $scope.bigCurrentPage + "&per_page=12&").then(function (response) {
     		$scope.lista = response.data;
     		if($scope.bigTotalItems == undefined)
     			$scope.bigTotalItems = parseInt($scope.lista.total_count);	
-		});
-	};
+    	});
+    };
 
 });
