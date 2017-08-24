bf622cfb211c3c4f45929a3d241c3fb59087ceaa
DESKTOP-57QE3ET\Matheus <matheus-gbn@hotmail.com>
2017-08-23 21:06:35 -0300

    Alterando o nome da váriavel 'detalhe' para 'detalhes'

--- controllers/detalheController.js
+++ controllers/detalheController.js
@@ -2,7 +2,7 @@ app.controller("detalheController", function($scope, $routeParams, $http){
 
 	$scope.buscarRepositorios = function(){
 		$http.get("https://api.github.com/repos/" + $routeParams.owner + "/" + $routeParams.repo).then(function (response) {
-    		$scope.detalhe = response.data;
+    		$scope.detalhes = response.data;
 		});
 	};
 });
